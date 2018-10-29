561ed1184e5f7130a6c788a8e5d70564addec97c
Mark Story <mark@sentry.io>
2018-10-29 11:55:01 -0400

    Add comment to make suspect commits fill up.

--- php/basic/basic.php
+++ php/basic/basic.php
@@ -2,7 +2,7 @@
 
 require __DIR__ . '/vendor/autoload.php';
 
-$client = new Raven_Client('https://<PUBLIC_DSN_KEY>:<PRIVATE_DSN_KEY>@sentry.io/<PROJ_ID>');
+$client = new Raven_Client('http://f0169d5419e44b28b6f4021c1b4c4d90@marksentry.ngrok.io/11');
 
 $error_handler = new Raven_ErrorHandler($client);
 $error_handler->registerExceptionHandler();
@@ -13,6 +13,5 @@ $client->user_context(array(
     'email' => 'testemail@eamail.com'
 ));
 
+// Do a dumb
 1/0; // throw uncaught error
-
-?>
